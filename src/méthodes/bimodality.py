103fce59d40a8197c8b9801c38f1304ee073dda9
Clément Haëck <52320542+Descanonge@users.noreply.github.com>
2023-02-01 20:02:08 +0100

    Update bimodality plot

--- src/méthodes/bimodality.py
+++ src/méthodes/bimodality.py
@@ -2,9 +2,9 @@
 import mypack.plots as mpp
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.offsetbox import AnchoredText
 from scipy.interpolate import interp1d
 
-plt.switch_backend('pgf')
 plt.style.use('thesis')
 
 np.random.seed(42)
@@ -26,8 +26,8 @@ g = 1./(std*np.sqrt(2*np.pi)) * np.exp(-.5*((x-avg)/std)**2)
 f_interp = interp1d(bins[:-1], h, kind='nearest',
                     bounds_error=False, fill_value=(h[0], h[-1]))
 
-fig, ax = plt.subplots(figsize=mpp.get_figsize(0.6, columnwidth=398))
-fig.subplots_adjust(left=0.2, bottom=0.2, right=0.98, top=0.98)
+fig, ax = plt.subplots(figsize=mpp.get_figsize(0.6, columnwidth=398),
+                       layout='constrained')
 
 ax.plot(bins[:-1], h, ds='steps-mid', color='k')
 ax.plot(x, g, color='red')
@@ -36,4 +36,18 @@ ax.fill_between(x, g, f_interp(x), alpha=0.3, color='red')
 ax.set_xlabel(r'Température [\unit{\degreeCelsius}]')
 ax.set_ylabel(r'Fréquence [N/A]')
 
-fig.savefig("./resources/méthodes/bimodality.pdf")
+an_pos = (x[380], g[380])
+an_text = AnchoredText(
+    "fit Gaussien", loc="upper right",
+    pad=0.1, borderpad=0.2
+)
+an_text.patch.set_edgecolor('none')
+ax.add_artist(an_text)
+ax.annotate("", an_pos, xycoords="data",
+            xytext=(0.5, 0.), textcoords=an_text,
+            arrowprops=dict(
+                arrowstyle='-', shrinkA=0, color='red', lw=1.2,
+                connectionstyle='angle,angleA=-90,angleB=45,rad=2'
+            ))
+
+fig.savefig("./resources/méthodes/bimodality.pdf", backend='pgf')
