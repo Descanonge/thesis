28e52ad7fe987697c3ee5ac9ef828444702fcfac
Clément Haëck <52320542+Descanonge@users.noreply.github.com>
2023-02-15 19:54:03 +0100

    Up
    New caption command

--- src/parse_log.sh
+++ src/parse_log.sh
@@ -3,8 +3,8 @@
 LOGFILE=$1
 LOG="$(pplatex -i "$LOGFILE" 2>&1)"
 
-if [ "$(echo "$LOG" | wc -l)" -ge 10 ]; then
-    OUTPUT="$(echo "$LOG" | head)\n...\n$(echo "$LOG" | tail)"
+if [ "$(echo "$LOG" | wc -l)" -ge 20 ]; then
+    OUTPUT="$(echo "$LOG" | head -n10)\n...\n$(echo "$LOG" | tail -n10)"
 else
     OUTPUT="$LOG"
 fi
